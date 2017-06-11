8d12384f68fe20e27a6b634ac0a9f34cb7e6aebb
george <g.obiero@carepay.co.ke>
2017-06-11 21:42:20 +0300

    Removed unused imports

--- src/test/java/ke/george/news/aggregation/NewsAggregationServiceApplicationTests.java
+++ src/test/java/ke/george/news/aggregation/NewsAggregationServiceApplicationTests.java
@@ -1,10 +1,7 @@
 package ke.george.news.aggregation;
 
-import com.jayway.restassured.RestAssured;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,14 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("unittest")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = NewsAggregationServiceApplication.class)
 public class NewsAggregationServiceApplicationTests {
-    @Value("${local.server.port:0}")
-    protected int port;
-
-    @Before
-    public void setUp() {
-        RestAssured.port = port;
-    }
-
     @Test
     public void contextLoads() {
     }
